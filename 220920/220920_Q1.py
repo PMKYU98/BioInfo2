2f7ec305e01d38b44fdb280c510f64efb8f5e38b
박명규 <pmkyu@PMK-MacBookAir.local>
2022-09-19 17:47:01 +0900

    220920 Q2 example success

--- 220920/220920_Q1.py
+++ 220920/220920_Q1.py
@@ -1,31 +1,15 @@
 money = 40
 Coins = [1,5,10,20,25,50]
 
+dicStack = {1: 1, 5: 1, 10: 1, 20: 1, 25: 1, 50: 1}
 
-def ADD_COIN(dicWallet, coin):
-    dicWallet[coin] += 1
+for i in range(1,money+1):
+    if i in dicStack: 
+        continue
+    else:
+        dicStack[i] = i
+        for j in range(1, i//2 +1):
+            temp = dicStack[j] + dicStack[i-j]
+            if temp < dicStack[i]: dicStack[i] = temp
 
-    if dicWallet[1] == 5:
-        dicWallet[1] = 0
-        dicWallet[5] += 1
-    if dicWallet[5] == 2:
-        dicWallet[5] = 0
-        dicWallet[10] += 1
-    if dicWallet[10] == 2:
-        dicWallet[10] = 0
-        dicWallet[20] += 1
-    if dicWallet[20] >= 1 and dicWallet[5] >= 1:
-        dicWallet[25] += 1
-        dicWallet[20] -= 1
-        dicWallet[5] -= 1
-    if dicWallet[25] == 2:
-        dicWallet[50] += 1
-        dicWallet[25] -= 1
-
-    return dicWallet
-
-dicWallet = {1: 0, 5: 0, 10: 0, 20: 0, 25: 0, 50: 0}
-for i in range(money):
-    dicWallet = ADD_COIN(dicWallet, 1)
-
-print(dicWallet)
+print(dicStack[money])
