24521dcb8f606c91fa08ef5e444c349195411f61
PMKYU98 <pmkyu98@snu.ac.kr>
2022-09-20 18:21:24 +0900

    220920 Q2 success

--- 220920/220920_Q2.py
+++ 220920/220920_Q2.py
@@ -1,15 +1,43 @@
 sInput = '''
-4 4
-1 0 2 4 3
-4 6 5 2 1
-4 4 5 2 1
-5 6 8 5 3
+18 15
+3 0 1 2 4 3 4 0 1 0 1 3 1 0 3 1
+1 3 3 1 2 0 0 2 3 0 2 4 3 1 3 2
+2 4 1 0 1 2 4 4 0 2 1 1 0 2 2 4
+0 4 4 1 1 1 2 3 1 0 4 3 1 0 4 0
+3 2 1 2 2 1 4 3 2 0 4 3 0 1 3 1
+3 1 3 0 3 4 1 2 0 0 0 0 1 2 1 0
+3 1 1 1 3 2 3 4 0 1 0 1 3 0 3 4
+4 4 1 4 2 4 2 3 3 0 2 1 3 2 4 4
+1 2 3 0 2 3 3 4 4 0 3 2 1 0 2 1
+0 0 2 2 4 4 0 1 3 1 0 4 4 0 0 4
+1 0 3 0 0 2 1 4 1 3 2 4 0 2 2 2
+0 0 1 3 4 0 3 1 3 3 1 0 1 3 0 3
+4 4 4 3 3 1 0 0 3 3 2 4 1 0 0 0
+4 0 0 3 3 2 1 4 1 3 0 0 3 0 4 3
+3 1 1 3 2 3 0 2 0 3 2 1 3 2 4 1
+1 0 4 3 3 3 4 3 3 0 1 0 3 4 3 0
+0 3 1 2 3 1 1 4 3 2 0 0 1 2 4 1
+1 3 2 0 1 3 4 0 2 0 2 4 2 3 4 3
 -
-3 2 4 0
-3 2 4 2
-0 7 3 3
-3 3 0 2
-1 3 2 2
+2 4 2 2 3 2 1 0 1 0 0 3 2 2 2
+4 4 0 1 3 2 4 2 1 2 0 3 0 0 2
+3 3 0 2 2 2 0 1 2 1 1 0 4 0 3
+3 0 2 0 0 1 2 3 3 0 4 0 4 3 3
+0 2 4 0 2 1 0 4 2 3 2 1 3 4 3
+1 1 4 4 0 2 3 3 3 0 4 1 2 2 0
+0 0 0 2 4 3 2 0 3 1 3 3 0 2 0
+1 3 4 3 3 3 4 1 4 2 2 0 3 2 3
+4 1 3 3 2 1 4 2 2 4 0 4 4 1 4
+2 0 1 3 0 4 2 0 3 1 1 2 2 3 2
+0 4 1 1 3 0 4 4 0 2 0 0 0 1 1
+1 1 2 1 0 4 2 4 4 0 4 3 2 2 4
+4 0 4 1 1 2 2 4 0 0 3 1 0 1 4
+0 2 1 1 2 4 2 2 2 3 3 4 4 3 4
+0 3 0 4 3 2 1 2 2 1 4 3 2 0 4
+1 3 0 0 3 1 1 4 3 1 3 2 1 0 0
+2 2 0 2 3 4 2 0 0 3 4 3 4 1 3
+4 1 2 0 1 1 4 4 3 2 1 4 0 3 3
+2 3 0 3 0 4 4 0 4 4 0 2 2 4 3
 '''     
 
 def PARSE_INPUT(sInput):
@@ -45,19 +73,19 @@ def CALCULATE(x, y, lDown, lRight, matScore):
     return max(scoreDown, scoreRight)
 
 def INITIALIZE(n, m, lDown, lRight):
-    matScore = [[0] * (n+1) for i in range(m+1)]
+    matScore = [[0] * (m+1) for i in range(n+1)]
 
-    for x in range(1,n+1):
+    for x in range(1,m+1):
         matScore[0][x] = CALCULATE(x, 0, lDown, lRight, matScore)
 
-    for y in range(1,m+1):
+    for y in range(1,n+1):
         matScore[y][0] = CALCULATE(0, y, lDown, lRight, matScore)
 
     return matScore
 
 def TOUR(n, m, lDown, lRight, matScore):
-    for y in range(1, m+1):
-        for x in range(1, n+1):
+    for y in range(1, n+1):
+        for x in range(1, m+1):
             matScore[y][x] = CALCULATE(x, y, lDown, lRight, matScore)
 
     return matScore
