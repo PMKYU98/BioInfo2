7673d95af6934e872686f25facb64da780a5e57c
PMKYU98 <pmkyu98@snu.ac.kr>
2022-09-27 18:47:16 +0900

    220927 Q1 success

--- 220927/220927_Q1.py
+++ 220927/220927_Q1.py
@@ -1,11 +1,59 @@
 sInput= '''
-0
-4
-0->1:7
-0->2:4
-2->3:2
-1->4:1
-3->4:3
+7
+17
+3->18:28
+3->11:34
+11->18:23
+3->14:23
+3->16:22
+8->16:22
+10->17:30
+10->14:37
+9->19:2
+15->19:23
+0->13:0
+5->10:37
+8->18:33
+5->14:3
+0->14:20
+6->7:13
+0->18:23
+2->4:30
+1->5:26
+2->19:25
+1->4:33
+1->8:2
+12->16:18
+1->2:1
+8->19:37
+1->17:12
+1->15:3
+1->12:35
+1->13:27
+4->9:33
+1->11:12
+9->14:37
+9->15:38
+0->11:17
+6->16:35
+0->9:1
+3->6:13
+2->14:7
+0->2:28
+1->19:36
+0->6:24
+0->5:32
+1->3:29
+4->19:31
+4->18:5
+4->17:26
+4->16:23
+7->15:21
+16->19:22
+7->12:2
+7->13:16
+13->17:20
+5->18:30
 '''
 
 class Graph:
@@ -24,8 +72,8 @@ class Graph:
         self.before[v].append((u, w))
 
     def search(self, u, d):
-        self.check()
-        print(self.next[u])
+        #self.check()
+        #print(self.next[u])
         for v, w in self.next[u]:
             self.longest[v] = max(self.longest[v], d + w)
             self.search(v, d + w)
@@ -44,6 +92,7 @@ class Graph:
                 if self.longest[v] == self.longest[current] - w:
                     current = v
 
+        print(self.longest[ed])
         print('->'.join([str(i) for i in reversed(answer)]))
             
 
@@ -72,6 +121,5 @@ graph = Graph(n)
 for edge in edges:
     graph.add_edge(edge[0], edge[1], edge[2])
 
-print(graph.next)
 graph.tour(st)
 graph.backtour(st, ed)
