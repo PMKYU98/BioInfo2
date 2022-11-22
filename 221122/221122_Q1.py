f968b647a3522a6efee8c58d3d5829aa868343d5
PMKYU <pmkyu98@kakao.com>
2022-11-22 11:07:39 +0900

    221122 Q1 example pass

--- 221122/221122_Q1.py
+++ 221122/221122_Q1.py
@@ -2,9 +2,28 @@ def PARSE(fInput):
     with open(fInput, 'r') as f:
         sInput = f.readline().strip()
 
-    return sInput
+    return list(sInput)
+
+def WRITE_OUTPUT(sBWT):
+    outputfile = '221122/221122_Q1_output.txt'
+    with open(outputfile, 'w') as f:
+        f.write(sBWT)
+
+    return
+
+def CYCLIC_ROTATION(lstInput):
+    matInput = []
+    for i in range(len(lstInput)):
+        suff_pref = lstInput[i:]
+        suff_pref.extend(lstInput[:i])
+        matInput.append(suff_pref)
+
+    return sorted(matInput)
+
+def BWT(matInput):
+    return ''.join([row[-1] for row in matInput])
 
 inputfile = '221122/221122_Q1_input1.txt'
-sInput = PARSE(inputfile)
-
-print(sInput)
+lstInput = PARSE(inputfile)
+matInput = CYCLIC_ROTATION(lstInput)
+WRITE_OUTPUT(BWT(matInput))
