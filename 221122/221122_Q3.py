46c70d51ee4c7653b4784e6a3cb49fd06a42de13
PMKYU <pmkyu98@kakao.com>
2022-11-22 12:33:12 +0900

    221122 Q3 example pass

---
+++ 221122/221122_Q3.py
@@ -0,0 +1,27 @@
+def PARSE(fInput):
+    with open(fInput, 'r') as f:
+        sInput = f.readline().strip()
+        idx = int(f.readline().strip())
+
+    return sInput, idx
+
+def POSITION(lst):
+    dicPos = {}
+    for i in range(len(lst)):
+        dicPos.setdefault(lst[i], [])
+        dicPos[lst[i]].append(i)
+
+    return dicPos
+
+def MOVE_CURSOR(cursor, char, dicPosFirst, dicPosLast):
+    idx = dicPosLast[char].index(cursor)
+    return dicPosFirst[char][idx]
+
+inputfile = '221122/221122_Q3_input1.txt'
+sLast, idx = PARSE(inputfile)
+sFirst = ''.join(sorted(sLast))
+
+dicPosFirst, dicPosLast = POSITION(sFirst), POSITION(sLast)
+
+answer = MOVE_CURSOR(idx, sLast[idx], dicPosFirst, dicPosLast)
+print(answer)
