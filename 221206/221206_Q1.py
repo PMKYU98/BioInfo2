41120ea5938aefe43ed70d3028acb928c4424adf
PMKYU <pmkyu98@kakao.com>
2022-12-06 12:58:59 +0900

    221206 Q1 let's start

--- 221206/221206_Q1.py
+++ 221206/221206_Q1.py
@@ -1,332 +1,143 @@
-from math import log10
+import numpy as np
+import pandas as pd
 
-def PARSE(fInput):
-    with open(fInput, 'r') as f:
-        String = f.readline().strip()
-        f.readline()
-        Threshold, Pseudocount = [float(x) for x in f.readline().strip().split() if x != '']
-        f.readline()
-        Alphabet = [character for character in f.readline().strip().split() if character != '']
-        f.readline()
+def construct_profile_hmm(threshold, alphabet, msa, pseudocount=0.01):
+    # Assign the states with available alignments over the threshold
+    matchno = 0
+    statemap = {}
+    for i in range(len(msa[0])):
+        unavailable = sum([seq[i] == '-' for seq in msa])
+        if unavailable / len(msa) < threshold:
+            matchno += 1
+            statemap[i] = f'M{matchno}'
+        else:
+            statemap[i] = f'I{matchno}'
 
-        lines = f.readlines()
-        lstAlign = [line.strip() for line in lines]
+    states = ['S', 'I0'] + [
+        f'{op}{i}' for i in range(1, matchno+1) for op in 'MDI'] + ['E']
+    state2i = {s: i for i, s in enumerate(states)}
+    sym2i = {s: i for i, s in enumerate(alphabet)}
 
-        return String, Threshold, Pseudocount, Alphabet, lstAlign
+    trcnt = np.zeros((len(states), len(states)), dtype=int)
+    emcnt = np.zeros((len(states), len(alphabet)), dtype=int)
 
-def WRITE_ANSWER(States, Alphabet, lstTransProb, lstEmitProb):
-    outputfile = '221206/221206_Q1_ProbTable.txt'
-    States[0] = 'S'
-    with open(outputfile, 'w') as f:
-        f.write('\t' + '\t'.join(States) + '\n')
-        for i in range(len(lstTransProb)):
-            l = [States[i]]
-            l.extend(['%.3f' % x for x in lstTransProb[i]])
-            f.write('\t'.join(l) + '\n')
-        
-        f.write('--------\n')
-        f.write('\t' + '\t'.join(Alphabet) + '\n')
-        for i in range(len(lstEmitProb)):
-            l = [States[i]]
-            l.extend(['%.3f' % x for x in lstEmitProb[i]])
-            f.write('\t'.join(l) + '\n')
-
-def IS_INSERTION(lstChar):
-    global Threshold
-    return lstChar.count('-') / len(lstChar) >= Threshold
-
-def SAY_IMD(lstIMDPrev, lstChar):
-    temp = []
-
-    if IS_INSERTION(lstChar):
-        for i in range(len(lstChar)):
-            if lstChar[i] == '-': temp.append('P' + lstIMDPrev[i][-1])
+    for seq in msa:
+        calls = ['S']
+        for i, sym in enumerate(seq):
+            state = statemap[i]
+            if sym != '-':
+                emcnt[state2i[state], sym2i[sym]] += 1
+                statecall = state
+            elif state[0] == 'M':
+                statecall = 'D' + state[1:]
             else:
-                if lstIMDPrev[i][0] == 'I': temp.append(lstIMDPrev[i])
-                else: temp.append('I' + lstIMDPrev[i][-1])
-
-        return True, temp
-    else:
-        for i in range(len(lstChar)):
-            if lstChar[i] == '-': temp.append('D' + str(int(lstIMDPrev[i][-1]) + 1))
-            else: temp.append('M' + str(int(lstIMDPrev[i][-1]) + 1))
-    
-        return False, temp
-
-def TRANS_COUNT(lstIMD):
-    dicTransNext = {}
-    for i in range(len(lstIMD)):
-        j = 0
-        k = 0
-        while j < len(lstIMD[i]) - 1:
-            k = k + 1
-            
-            if lstIMD[i][k][0] == 'P':
                 continue
-            else:
-                dicTransNext.setdefault(lstIMD[i][j], [])
-                dicTransNext[lstIMD[i][j]].append(lstIMD[i][k])
-                j = k
-        
-    return dicTransNext
 
-def EMIT_COUNT(lstAlign, lstIMD):
-    dicEmit = {}
-    for idx in range(len(lstAlign)):
-        seq = lstAlign[idx]
-        IMD = lstIMD[idx][1:-1]
-        for step in range(len(IMD)):
-            state = IMD[step]
-            if state[0] == 'I' or state[0] == 'M':
-                dicEmit.setdefault(state, [])
-                dicEmit[state].append(seq[step])
+            calls.append(statecall)
 
-    return dicEmit
+        calls.append('E')
 
-def CALC_TRANS(lstAlign):
-    lstIMD = [['S0'] for _ in range(len(lstAlign))]
-    temp = ['S0'] * len(lstAlign)
-    insertion = 0
-    for i in range(len(lstAlign[0])):
-        inserted, temp = SAY_IMD(temp, [sequence[i] for sequence in lstAlign])
-        for j in range(len(temp)):
-            lstIMD[j].append(temp[j])
-        if inserted: insertion += 1
+        for a, b in zip(calls, calls[1:]):
+            trcnt[state2i[a], state2i[b]] += 1
 
-    for j in range(len(temp)):
-            lstIMD[j].append('E')
+    trprob = trcnt / trcnt.sum(axis=1).clip(1)[:, np.newaxis]
+    emprob = emcnt / emcnt.sum(axis=1).clip(1)[:, np.newaxis]
 
-    dicTransNext = TRANS_COUNT(lstIMD)
-    
-    States = ['S0']
-    for step in range(len(lstAlign[0]) - insertion):
-        States.append('I' + str(step))
-        States.append('M' + str(step + 1))
-        States.append('D' + str(step + 1))
-    States.append('I' + str(step + 1))
-    States.append('E')
+    # Add pseudocounts
+    for state in states:
+        if state[0] in 'SE':
+            continue
 
-    lstTransProb = []
-    for l in States:
-        if l == 'E':
-            lstTransProb.append([0.0] * len(States))
-        elif l in dicTransNext:
-            row = []
-            count = len(dicTransNext[l])
-            nexts = dicTransNext[l]
+        snum = int(state[1:])
+        snext = snum + 1
 
-            for j in States:
-                row.append(nexts.count(j) / count)
-            lstTransProb.append(row)
+        # Add pseudocounts to transition probabilities
+        if snum == matchno:
+            outgoing = [f'I{snum}', 'E']
         else:
-            lstTransProb.append([0.0] * len(States))
-            
-    return States, lstIMD, lstTransProb
+            outgoing = [f'I{snum}', f'M{snext}', f'D{snext}']
 
-def CALC_EMIT(Alphabet, States, lstIMD, lstAlign):
-    dicEmit = EMIT_COUNT(lstAlign, lstIMD)
+        outgoing = [state2i[s] for s in outgoing]
+        probspace = state2i[state], outgoing
+        newprob = trprob[probspace] + pseudocount
+        trprob[probspace] = newprob / newprob.sum()
 
-    lstEmitProb = []
-    for l in States:
-        if l in dicEmit:
-            row = []
-            count = len(dicEmit[l])
-            nexts = dicEmit[l]
-
-            for j in Alphabet:
-                row.append(nexts.count(j) / count)
-            lstEmitProb.append(row)
-        else:
-            lstEmitProb.append([0] * len(Alphabet))
-
-    return lstEmitProb
-
-def PSEUDOCOUNT(States, Alphabet, Pseudocount, lstTransProb, lstEmitProb):
-    lstEmitProbNew = []
-    for i in range(len(States)):
-        if States[i][0] in ['S', 'D', 'E']: lstEmitProbNew.append([0] * len(Alphabet))
-        else:
-            sum_prob = sum(lstEmitProb[i]) + Pseudocount * len(Alphabet)
-            temp = [(prob + Pseudocount) / sum_prob for prob in lstEmitProb[i]]
-            lstEmitProbNew.append(temp)
-
-    lstTransProbNew = []
-    final = States[-2][-1]
-    for i in range(len(States)):
-        if States[i] == 'E':
-            lstTransProbNew.append(lstTransProb[i])
-            return lstEmitProbNew, lstTransProbNew
-
-        num = States[i][1]
-        if num == final:
-            st = 3 * int(num) + 1
-            ed = 3 * int(num) + 2
-            sum_prob = sum(lstTransProb[i][st:ed+1]) + Pseudocount * 2
-            temp = lstTransProb[i]
-            temp[st] = (lstTransProb[i][st] + Pseudocount) / sum_prob
-            temp[st+1] = (lstTransProb[i][st+1] + Pseudocount) / sum_prob
-            
-        else:
-            st = 3 * int(num) + 1
-            ed = 3 * int(num) + 3
-            sum_prob = sum(lstTransProb[i][st:ed+1]) + Pseudocount * 3
-            temp = lstTransProb[i]
-            temp[st] = (lstTransProb[i][st] + Pseudocount) / sum_prob
-            temp[st+1] = (lstTransProb[i][st+1] + Pseudocount) / sum_prob
-            temp[st+2] = (lstTransProb[i][st+2] + Pseudocount) / sum_prob
-
-        lstTransProbNew.append(temp)
-
-def ZERO_COL(lstTransProb, States):
-    tempScores, tempBacktrack = [0], ['O']
-    for state in States:
+        # Add pseudocounts to emission probabilities
         if state[0] == 'D':
-            print(state)
-            print(tempScores)
-            if state[1] == '1':
-                tempScores.append(log10(lstTransProb[0][3]))
-                tempBacktrack.append('S')
-                continue
-            i = int(state[1]) - 1
-            print(lstTransProb[States.index('D' + str(i))][States.index(state)])
-            tempScores.append(tempScores[-1] + log10(lstTransProb[States.index('D' + str(i))][States.index(state)]))
-            tempBacktrack.append('D' + str(i))
+            continue
 
-    return tempScores, tempBacktrack
+        si = state2i[state]
+        newprob = emprob[si] + pseudocount
+        emprob[si] = newprob / newprob.sum()
 
-def FIRST_COL(lstScores, lstTransProb, lstEmitProb, String, Alphabet, States):
-    tempScores, tempBacktrack = [], []
-    zero_col = lstScores[0]
-    for i in range(1, len(States) - 1):
-        if i == 1 or i == 2: 
-            tempScores.append(log10(lstTransProb[0][i]) + log10(lstEmitProb[i][Alphabet.index(String[0])]))
-            tempBacktrack.append('S')
-        elif i == 3:
-            tempScores.append(tempScores[0] + log10(lstTransProb[1][3]))
-            tempBacktrack.append('I0')
-        else:
-            if States[i][0] == 'D':
-                # from M: i - 4, from D: i - 3, from I: i - 2
-                j = str(int(States[i][1]) - 1)
-                match_j, del_j, ins_j = 'M'+j, 'D'+j, 'I'+j
-                match_score = tempScores[i - 1 - 4] + log10(lstTransProb[States.index(match_j)][i])
-                del_score = tempScores[i - 1 - 3] + log10(lstTransProb[States.index(del_j)][i])
-                ins_score = tempScores[i - 1 - 2] + log10(lstTransProb[States.index(ins_j)][i])
+    return trprob, emprob, states
 
-                if match_score >= del_score:
-                    if match_score >= ins_score:
-                        tempScores.append(match_score)
-                        tempBacktrack.append(match_j)
-                    else:
-                        tempScores.append(ins_score)
-                        tempBacktrack.append(ins_j)
-                else:
-                    if del_score >= ins_score:
-                        tempScores.append(del_score)
-                        tempBacktrack.append(del_j)
-                    else:
-                        tempScores.append(ins_score)
-                        tempBacktrack.append(ins_j)
-            else:
-                if States[i][0] == 'I': j = int(States[i][1])
-                elif States[i][0] == 'M': j = int(States[i][1]) - 1
-                tempScores.append(zero_col[j] + \
-                    log10(lstTransProb[States.index('D' + str(j))][i]) + \
-                    log10(lstEmitProb[i][Alphabet.index(String[0])]))
-                tempBacktrack.append('D' + str(j))
+def PARSE(inputfile):
+    fInput = open(inputfile, 'r')
+    inputdata = fInput.readlines()
+    fInput.close()
 
-    return tempScores, tempBacktrack
+    sequence = inputdata[0].strip()
 
-def OTHER_COL(n, lstScores, lstTransProb, lstEmitProb, String, Alphabet, States):
-    tempScores, tempBacktrack = [], []
-    zero_col = lstScores[-1]
-    for i in range(1, len(States) - 1):
-        if i == 1 or i == 2: 
-            tempScores.append(log10(lstTransProb[1][i]) + log10(lstEmitProb[i][Alphabet.index(String[n])]))
-            tempBacktrack.append('I0')
-        elif i == 3:
-            tempScores.append(tempScores[0] + log10(lstTransProb[1][3]))
-            tempBacktrack.append('I0')
-        else:
-            if States[i][0] == 'D':
-                # from M: i - 4, from D: i - 3, from I: i - 2
-                j = str(int(States[i][1]) - 1)
-                match_j, del_j, ins_j = 'M'+j, 'D'+j, 'I'+j
-                match_score = tempScores[i - 1 - 4] + log10(lstTransProb[States.index(match_j)][i])
-                del_score = tempScores[i - 1 - 3] + log10(lstTransProb[States.index(del_j)][i])
-                ins_score = tempScores[i - 1 - 2] + log10(lstTransProb[States.index(ins_j)][i])
+    secondline = inputdata[2].strip().split()
+    threshold = float(secondline[0])
+    pseudocount = float(secondline[1])
 
-            elif States[i][0] == 'I':
-                j = str(int(States[i][1]))
-                match_j, del_j, ins_j = 'M'+j, 'D'+j, 'I'+j
-                match_score = zero_col[i - 3] + log10(lstTransProb[States.index(match_j)][i]) + log10(lstEmitProb[i][Alphabet.index(String[n])])
-                del_score = zero_col[i - 2] + log10(lstTransProb[States.index(del_j)][i]) + log10(lstEmitProb[i][Alphabet.index(String[n])])
-                ins_score = zero_col[i - 1] + log10(lstTransProb[States.index(ins_j)][i]) + log10(lstEmitProb[i][Alphabet.index(String[n])])
+    alphabet = inputdata[4].split()
+    alphabet2i = {a: i for i, a in enumerate(alphabet)}
 
-            elif States[i][0] == 'M': 
-                j = str(int(States[i][1]) - 1)
-                match_j, del_j, ins_j = 'M'+j, 'D'+j, 'I'+j
-                match_score = zero_col[i - 4] + log10(lstTransProb[States.index(match_j)][i]) + log10(lstEmitProb[i][Alphabet.index(String[n])])
-                del_score = zero_col[i - 3] + log10(lstTransProb[States.index(del_j)][i]) + log10(lstEmitProb[i][Alphabet.index(String[n])])
-                ins_score = zero_col[i - 2] + log10(lstTransProb[States.index(ins_j)][i]) + log10(lstEmitProb[i][Alphabet.index(String[n])])
+    msa = [l.strip() for l in inputdata[6:]]
 
-            if match_score >= del_score:
-                if match_score >= ins_score:
-                    tempScores.append(match_score)
-                    tempBacktrack.append(match_j)
-                else:
-                    tempScores.append(ins_score)
-                    tempBacktrack.append(ins_j)
-            else:
-                if del_score >= ins_score:
-                    tempScores.append(del_score)
-                    tempBacktrack.append(del_j)
-                else:
-                    tempScores.append(ins_score)
-                    tempBacktrack.append(ins_j)
+    trprob, emprob, states = construct_profile_hmm(threshold, alphabet, msa, pseudocount)
 
-    return tempScores, tempBacktrack
+    return sequence, alphabet2i, trprob, emprob, states
 
-def VITERBI(String, lstTransProb, lstEmitProb, States):
-    tempScores, tempBacktrack = ZERO_COL(lstTransProb, States)
-    lstScores, lstBacktrack = [tempScores], [tempBacktrack]
+def phmm_topological_order(obslen, states):
+    state2i = {stname: i for i, stname in enumerate(states)}
+    nmatches = sum([s[0] == 'M' for s in states])
 
-    tempScores, tempBacktrack = FIRST_COL(lstScores, lstTransProb, lstEmitProb, String, Alphabet, States)
-    lstScores.append(tempScores)
-    lstBacktrack.append(tempBacktrack)
+    # Generate the first column for initial deletions
+    for k in range(1, nmatches + 1):
+        yield (0, state2i[f'D{k}'])
 
-    for n in range(len(String)):
-        tempScores, tempBacktrack = OTHER_COL(n, lstScores, lstTransProb, lstEmitProb, String, Alphabet, States)
-        lstScores.append(tempScores)
-        lstBacktrack.append(tempBacktrack)
+    # Generate toplogical ordering for the remaining columns
+    for i in range(1, obslen + 1):
+        for k in range(1, len(states) - 1): # except S, E
+            yield (i, k)
 
-    return lstScores, lstBacktrack
+    # Generate 'E' state
+    yield (obslen, state2i['E'])
 
-def BACKTRACK(States, lstScores, lstBacktrack):
-    _States = States[1:-1]
+def viterbi(observation, initprob, trprob, emprob):
+    nstates = len(initprob)
 
-    temp = lstScores[-1][-3:]
-    temp_max = temp.index(max(temp))
-    curr = _States[-3:][temp_max]
+    probprod = np.zeros([nstates, len(observation)], dtype=np.float64)
+    backtrack = np.zeros([nstates, len(observation)], dtype=np.int16)
 
-    backtrack = [curr]
-    i = -1
-    while curr != 'S':
-        idx = _States.index(curr)
-        curr = lstBacktrack[i][idx]
-        backtrack.append(curr)
-        i -= 1
+    probprod[:, 0] = emprob[:, observation[0]] * initprob
 
-    return ' '.join(reversed(backtrack[:-1]))
+    for j, obs in enumerate(observation[1:], 1):
+        for s in range(nstates):
+            k = np.argmax(probprod[:, j-1] * trprob[:, s] * emprob[s, obs])
+            probprod[s, j] = probprod[k, j-1] * trprob[k, s] * emprob[s, obs]
+            backtrack[s, j] = k
+
+    best_path = []
+    k = np.argmax(probprod[:, -1])
+    for j in range(len(observation) - 1, -1, -1):
+        best_path.append(k)
+        k = backtrack[k, j]
+    return best_path[::-1]
 
 
-inputfile = '221206/221206_Q1_input2.txt'
-String, Threshold, Pseudocount, Alphabet, lstAlign = PARSE(inputfile)
-States, lstIMD, lstTransProb = CALC_TRANS(lstAlign)
+inputfile = '221206/221206_Q1_input1.txt'
+sequence, alphabet2i, trprob, emprob, states = PARSE(inputfile)
+observation = [alphabet2i[s] for s in sequence]
+initprob = [1 / len(states)] * len(states)
 
-lstEmitProb = CALC_EMIT(Alphabet, States, lstIMD, lstAlign)
-lstEmitProb, lstTransProb = PSEUDOCOUNT(States, Alphabet, Pseudocount, lstTransProb, lstEmitProb)
-WRITE_ANSWER(States, Alphabet, lstTransProb, lstEmitProb)
+decoded_states = viterbi(observation, initprob, trprob, emprob)
+print(' '.join([states[i] for i in decoded_states]))
 
-lstScores, lstBacktrack = VITERBI(String, lstTransProb, lstEmitProb, States)
-print(BACKTRACK(States, lstScores, lstBacktrack))
+obslen = len(observation)
+for state in phmm_topological_order(obslen, states):
+    print(state)
